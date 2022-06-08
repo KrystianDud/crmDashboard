3296fb34424de2059c7cbf846064d79e2f6fb6bd
KrystianDud <78433587+KrystianDud@users.noreply.github.com>
2022-06-08 14:36:57 +0100

    add global test variables b4 backend implementation

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,3 +4,9 @@ require('./bootstrap');
 
 // React Components
 require('./app')
+
+// Declare global variables for testing here to be able quickly catch them during the development
+
+// use this to simluate user type.
+// when true, act like a service, false is client
+const productTest = true;
