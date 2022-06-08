d61a89e793c5451140404117ea54ff814afba784
KrystianDud <78433587+KrystianDud@users.noreply.github.com>
2022-06-08 14:36:32 +0100

    input enhancement

--- resources/js/Components/Global/Input.js
+++ resources/js/Components/Global/Input.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './index.css'
+import '../../../css/globals.css'
 
 export default function Input({type, margin, label, onChange, id}) {
     return (
-        <form className='formHandler' style={{margin: `${margin}`}}>
+        <form className={type == 'checkbox' ? 'formHandlerHorizontal mt15' : 'formHandler'} style={{margin: `${margin}`}}>
             <label className="inputLabel">{label}</label>
             <input id={id} className="inputClass" type={type} onChange={(e) => onChange(e)}/>
-
-
         </form>
     )
 }
