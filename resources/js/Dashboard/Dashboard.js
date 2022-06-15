73f2860b1fa70907df6bebe1b51853aaa2b24eb9
KrystianDud <78433587+KrystianDud@users.noreply.github.com>
2022-06-15 20:07:53 +0100

    reminder rendering tweak

--- resources/js/Dashboard/Dashboard.js
+++ resources/js/Dashboard/Dashboard.js
@@ -1,16 +1,54 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import './index.css';
 import '../../css/globals.css'
 
 import Card from '../Components/Card/Index'
 import History from '../Components/History/index'
 import Reminder from '../Components/Reminder';
-
-import { UserDataContext } from '../app'
+import Invite from '../Components/Invite';
+import { UserDataContext, CompanyDataContext } from '../app'
+import { isEmpty } from 'lodash';
 
 export default function Dashboard({ user, activateModal }) {
     const { userData, setUserData } = useContext(UserDataContext);
-    console.log('user context', userData)
+    const { companyData, setCompanyData } = useContext(CompanyDataContext);
+
+    useEffect(() => {
+        checkReminders()
+    }, [userData])
+    console.log('dashboard rerender', companyData)
+    const checkReminders = () => {
+        // In this function we check onboarding users, especially admins to see if they did everything to make sure that platform runs smoothly
+        // using axios we will connect to the server to seek confirmation if things such as: 
+        // company details, ADD MORE REASONS HERE
+        // Then an array will be created and passed to the reminder component to display one after another
+        // Reminders should be hardcoded and the content provided inside of the Component using switch
+        // Reminders should be part of the userData and be available through the context hook
+
+        // Provide dummy context until the Api is in place
+        // const reminderContext = ['company'];
+        // const userItems = userData;
+
+        // userItems.reminderContext = reminderContext;
+        // setUserData(userItems)
+    }
+
+    const displayReminders = (
+        userData.type == 'client' && userData.reminderContext && userData.reminderContext.length > 0 ?
+            <div className="userReminder m20">
+                {/* 
+                provide to user reminder a number of things to do in a specific order
+                to ensure that client can carry out specific actions.
+                ### First, fill in company details. this should open a modal and provide billing address and etc.
+                This will not include email confirmation.
+            */}
+                <Reminder
+                    reminderContext={userData.reminderContext}
+                    onStart={(modalData) => activateModal(modalData)}
+                />
+            </div>
+            : null
+    )
 
     return (
         <div className="viewWindow">
@@ -18,17 +56,9 @@ export default function Dashboard({ user, activateModal }) {
                 <h2>{`Welcome ${user}!`}</h2>
             </div>
 
-            <div className="userReminder m20">
-                {/* provide to user reminder a number of things to do in a specific order
-                    to ensure that client can carry out specific actions.
-                    ### First, fill in company details. this should open a modal and provide billing address and etc.
-                    This will not include email confirmation.
-                */}
-                <Reminder
-                    userData={userData}
-                    onStart={(modalData) => activateModal(modalData)}
-                />
-            </div>
+            {displayReminders}
+            {/* invite can be displayed only when the admin has created company */}
+            {companyData.id && userData.privilege == 'admin' ? <Invite company={companyData} /> : null}
 
             <div className='card-inline'>
                 {cardElements.map(({ icon, title, value }, index) => (
@@ -40,6 +70,8 @@ export default function Dashboard({ user, activateModal }) {
                     />
                 ))}
             </div>
+
+
             <History />
         </div>
     )
