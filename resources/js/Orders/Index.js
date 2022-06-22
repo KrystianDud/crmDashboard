1e0279ac6e579c8e81cf6d622a1ed5939bd16a1c
KrystianDud <78433587+KrystianDud@users.noreply.github.com>
2022-06-22 21:17:23 +0100

    connect the functionality of table with backend and remove dummy data

--- resources/js/Orders/Index.js
+++ resources/js/Orders/Index.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './index.css';
-import { v4 as uuidv4 } from 'uuid';
-
 import { UserDataContext } from '../app'
 
-import Tablist from '../Components/Tablist';
 import Table from '../Components/Table';
 import ButtonRectangle from '../Components/IconButtons/ButtonRectangle';
+import Button from '../Components/Button';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilter, faPaperclip, faList } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
-export default function Orders() {
+export default function Orders({ user }) {
     const { userData, setUserData } = useContext(UserDataContext);
 
     const [x, setX] = useState(0)
@@ -21,68 +20,29 @@ export default function Orders() {
     const [filterView, setFilterView] = useState(false)
     const [currentList, setCurrentlist] = useState()
 
-    // The reason for hardcoded columns is becuase sometime there might be a 
+    // The reason for hardcoded columns is becuase sometimes there might be a 
     // chance that the name of the item for the table might have undescore which will result in bad UX
-    const [columns, setColumns] = useState([
-        {
-            name: 'Id',
-            keyName: 'id'
-        },
-        {
-            name: 'Customer',
-            keyName: 'customer'
-        },
-        {
-            name: 'Order',
-            keyName: 'order'
-        },
-        {
-            name: 'Date',
-            keyName: 'date'
-        },
-        {
-            name: 'Price',
-            keyName: 'price'
-        },
-    ]);
+    // Later this should be moved to backend so it can return the tables based on the user verification approach
+    const [columnsClient, setColumnsClient] = useState();
 
-    useEffect(
-        () => {
-            const update = (e) => {
-                if (e.target.closest('path') || e.target.closest('svg')) return
+    useEffect(() => {
+        console.log('parmas', user.id, user.company_id)
 
-                if (e.target.className.includes('listBoxButton') && !filterView) {
-                    let left = e.target.offsetLeft
-                    let top = e.target.offsetTop
-                    let height = e.target.offsetHeight
-                    let width = e.target.offsetWidth
-                    if (e.target.offsetLeft > (window.innerWidth / 2)) {
-                        setX((left - width) + 'px')
-                        setY(top + height + 'px')
-                    }
-                    else {
-                        setX(left + width + 'px')
-                        setY(top + height + 'px')
-                    }
-                    setFilterView(true)
-                }
-                else if (e.target.className.includes('listBox')) setFilterView(true)
-                else if ((!e.target.className.includes('listBoxButton') || !e.target.className.includes('listBox')) && filterView) setFilterView(false)
-                else {
-                    setFilterView(false)
-                }
-
+        axios.get('/api/orders', {
+            params: {
+                user_id: user.id,
+                company_id: user.company_id
             }
-            window.addEventListener('mousemove', update)
-            window.addEventListener('touchmove', update)
-            return () => {
-                window.removeEventListener('mousemove', update)
-                window.removeEventListener('touchmove', update)
-                setFilterView(false)
-            }
-        },
-        [setX, setY]
-    )
+
+        })
+            .then((response) => {
+                console.log(response)
+                setCurrentlist(response.data.data)
+                setColumnsClient(response.data.columns)
+            })
+
+    }, [user])
+
 
     const filterList = (filterId) => {
         // provide the functionality to show the small dialog
@@ -105,6 +65,21 @@ export default function Orders() {
     const viewType = () => {
     }
 
+    // When the options are set to true on the table we can provide them in the parent
+    // Due to the fact that not always table will have this functionality and also they might need
+    // to be configured in different ways depending on the client and service needs
+    const provideOptions = (
+        <Button
+            text={'View'}
+            type={'contained'}
+            disabled={false}
+            color={'normal'}
+            size={'sm'}
+            icon={null}
+            callback={() => editItem(index)}
+        />
+    );
+
     return (
         <div style={{ height: '90%' }} >
             <div className='mainView'>
@@ -114,86 +89,17 @@ export default function Orders() {
                         onClick={filterList}
                     />
                 </div>
-                <Table
-                    columns={columns}
-                    list={orderHistory}
-                    showDetails={null}
-                    options={false}
-                    editItem={null}
-
-                    user={userData}
-
-                />
+                {currentList && columnsClient ?
+                    <Table
+                        columns={columnsClient}
+                        list={currentList}
+                        showDetails={null}
+                        options={true}
+                        provideOptions={provideOptions}
+                        editItem={null}
+                        user={userData}
+                    /> : null}
             </div>
         </div>
     )
-}
-
-const tabs = [
-    {
-        name: 'All Orders'
-    },
-    {
-        name: 'Declined'
-    }
-]
-
-
-const orderHistory = [
-    {
-        id: uuidv4(),
-        customer: 'Chenai Novak',
-        order: '',
-        date: '10/04/2022',
-        price: '$100'
-    },
-    {
-        id: uuidv4(),
-        customer: 'Lexi-May Travis',
-        order: '',
-        date: '10/04/2022',
-        price: '$100'
-    },
-    {
-        id: uuidv4(),
-        customer: 'Maison Bryan',
-        order: '',
-        date: '10/04/2022',
-        price: '$100'
-    },
-    {
-        id: uuidv4(),
-        customer: 'Anjali Talley',
-        order: '',
-        date: '10/04/2022',
-        price: '$100'
-    },
-    {
-        id: uuidv4(),
-        customer: 'Reese Delaney',
-        order: '',
-        date: '10/04/2022',
-        price: '$100'
-    },
-    {
-        id: uuidv4(),
-        customer: 'Ella-Rose Schofield',
-        order: '',
-        date: '10/04/2022',
-        price: '$100'
-    },
-    {
-        id: uuidv4(),
-        customer: 'Kendra Dolan',
-        order: '',
-        date: '10/04/2022',
-        price: '$100'
-    },
-    {
-        id: uuidv4(),
-        customer: 'Sila Austin',
-        order: '',
-        date: '10/04/2022',
-        price: '$100'
-    },
-]; 
+} 
