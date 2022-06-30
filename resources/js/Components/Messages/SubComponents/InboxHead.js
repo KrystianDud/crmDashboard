a600a93102424492af5c592cbeecda282970f91f
KrystianDud <78433587+KrystianDud@users.noreply.github.com>
2022-06-30 17:29:01 +0100

    use global css

--- resources/js/Components/Messages/SubComponents/InboxHead.js
+++ resources/js/Components/Messages/SubComponents/InboxHead.js
@@ -11,19 +11,19 @@ export default function InboxHead({ message, onClick, selected }) {
 
     return (
         <div
-            className={`${selected ? 'background-normal-secondary' : 'background-white hover-grey-primary'} messages-inbox-head flexRow alignCenter flexBetween mb5`}
+            className={`${selected ? 'background-normal-secondary' : 'background-white hover-grey-primary'} messages-inbox-head flexRow alignCenter justifyStarts mb5`}
             onClick={onClick}
         >
             <div
-                className="messages-inbox-profile m5"
+                className="messages-inbox-profile mt5 mb5 mInline15"
                 style={{ background: `url(${message.lines[0].avatar.slice(6)})`, backgroundSize: 'contain' }}
             />
-            <div className="messages-inbox-snippets flexColumn alignStart ">
-                <div className="messages-inbox-nameTime flexRow flexBetween ">
+            <div className="messages-inbox-snippets flexColumn alignStart justifyStart fs-07r ">
+                <div className="messages-inbox-nameTime flexRow flexBetween fs-08r w100">
                     <p className='m0 fw4'>{message.lines[0].name} {message.lines[0].surname}</p>
                     <Moment format="ddd, hh:mm A" date={message.created_at} className="fw3" />
                 </div>
-                <p className='m0 fw3'>Subject: {message.subject}</p>
+                <p className='m0 fw3 fs-07r'>{message.subject}</p>
             </div>
         </div>
     )
